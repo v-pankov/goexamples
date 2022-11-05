4fe8392b4874b641596f218f29bece3abed24209
Vladimir Pankov <vdrpkv@gmail.com>
2022-11-06 01:40:17 +0300

    Fill in inmem repository scaffoldings

--- internal/chat/drivers/repository/inmem/inmem.go
+++ internal/chat/drivers/repository/inmem/inmem.go
@@ -2,11 +2,15 @@ package inmem
 
 import (
 	"context"
+	"fmt"
 	"sync"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/vdrpkv/goexamples/internal/chat/domain/message"
 	"github.com/vdrpkv/goexamples/internal/chat/domain/session"
 	"github.com/vdrpkv/goexamples/internal/chat/domain/user"
+	"github.com/vdrpkv/goexamples/internal/pkg/entity"
 
 	messageSendUsecase "github.com/vdrpkv/goexamples/internal/chat/domain/message/usecase/send/usecase"
 	messageSendValidator "github.com/vdrpkv/goexamples/internal/chat/domain/message/usecase/send/validator"
@@ -37,9 +41,10 @@ func New() Repository {
 
 type repo struct {
 	sync.Mutex
-	userMap    map[user.ID]*user.Entity
-	sessionMap map[session.ID]*session.Entity
-	messageMap map[message.ID]*message.Entity
+	userMap       map[user.ID]*user.Entity
+	sessionMap    map[session.ID]*session.Entity
+	messageMap    map[message.ID]*message.Entity
+	nextMessageID int64
 }
 
 func (r *repo) SessionLoginUsecaseRepository() sessionLoginUsecase.Repository {
@@ -67,11 +72,43 @@ type sessionLoginUsecaseRepositoryAdapter struct {
 }
 
 func (a sessionLoginUsecaseRepositoryAdapter) CreateOrFindUser(ctx context.Context, userName user.Name) (*user.Entity, error) {
-	return nil, nil
+	a.Lock()
+	val, err := a.createOrFindUser(ctx, userName)
+	a.Unlock()
+	return val, err
 }
 
 func (a sessionLoginUsecaseRepositoryAdapter) CreateActiveSession(ctx context.Context, userID user.ID) (*session.Entity, error) {
+	a.Lock()
+	val, err := a.createActiveSession(ctx, userID)
+	a.Unlock()
+	return val, err
+}
+
+func (a sessionLoginUsecaseRepositoryAdapter) createOrFindUser(_ context.Context, userName user.Name) (*user.Entity, error) {
+	for _, userEntity := range a.repo.userMap {
+		if userEntity.Name == userName {
+			return userEntity, nil
+		}
+	}
 	return nil, nil
+}
+
+func (a sessionLoginUsecaseRepositoryAdapter) createActiveSession(_ context.Context, userID user.ID) (*session.Entity, error) {
+	var (
+		timeNow       = time.Now()
+		sessionEntity = &session.Entity{
+			Entity: entity.Entity{
+				CreatedAt: timeNow,
+				UpdatedAt: timeNow,
+			},
+			ID:     session.ID(uuid.New().String()),
+			UserID: userID,
+			Active: true,
+		}
+	)
+	a.sessionMap[sessionEntity.ID] = sessionEntity
+	return sessionEntity, nil
 }
 
 type sessionLogoutUsecaseRepositoryAdapter struct {
@@ -79,6 +116,19 @@ type sessionLogoutUsecaseRepositoryAdapter struct {
 }
 
 func (a sessionLogoutUsecaseRepositoryAdapter) DeactivateSession(ctx context.Context, sessionID session.ID) error {
+	a.Lock()
+	err := a.deactivateSession(ctx, sessionID)
+	a.Unlock()
+	return err
+}
+
+func (a sessionLogoutUsecaseRepositoryAdapter) deactivateSession(ctx context.Context, sessionID session.ID) error {
+	sessionEntity := a.sessionMap[sessionID]
+	if sessionEntity == nil {
+		return fmt.Errorf("session is not found")
+	}
+
+	sessionEntity.Active = false
 	return nil
 }
 
@@ -87,7 +137,14 @@ type sessionLogoutValidatorRepositoryAdapter struct {
 }
 
 func (a sessionLogoutValidatorRepositoryAdapter) FindSession(ctx context.Context, sessionID session.ID) (*session.Entity, error) {
-	return nil, nil
+	a.Lock()
+	val, err := a.findSession(ctx, sessionID)
+	a.Unlock()
+	return val, err
+}
+
+func (a sessionLogoutValidatorRepositoryAdapter) findSession(ctx context.Context, sessionID session.ID) (*session.Entity, error) {
+	return a.sessionMap[sessionID], nil
 }
 
 type messageSendUsecaseRepositoryAdapter struct {
@@ -102,7 +159,35 @@ func (a messageSendUsecaseRepositoryAdapter) CreateMessage(
 	*message.Entity,
 	error,
 ) {
-	return nil, nil
+	a.Lock()
+	val, err := a.createMessage(ctx, authorUserSessionID, messageText)
+	a.Unlock()
+	return val, err
+}
+
+func (a messageSendUsecaseRepositoryAdapter) createMessage(
+	ctx context.Context,
+	authorUserSessionID session.ID,
+	messageText string,
+) (
+	*message.Entity,
+	error,
+) {
+	a.nextMessageID++
+	var (
+		timeNow       = time.Now()
+		messageEntity = &message.Entity{
+			Entity: entity.Entity{
+				CreatedAt: timeNow,
+				UpdatedAt: timeNow,
+			},
+			ID:        message.ID(a.nextMessageID),
+			SessionID: authorUserSessionID,
+			Text:      messageText,
+		}
+	)
+	a.messageMap[messageEntity.ID] = messageEntity
+	return messageEntity, nil
 }
 
 type messageSendValidatorRepositoryAdapter struct {
@@ -110,5 +195,12 @@ type messageSendValidatorRepositoryAdapter struct {
 }
 
 func (a messageSendValidatorRepositoryAdapter) FindSession(ctx context.Context, sessionID session.ID) (*session.Entity, error) {
-	return nil, nil
+	a.Lock()
+	val, err := a.findSession(ctx, sessionID)
+	a.Unlock()
+	return val, err
+}
+
+func (a messageSendValidatorRepositoryAdapter) findSession(ctx context.Context, sessionID session.ID) (*session.Entity, error) {
+	return a.sessionMap[sessionID], nil
 }
