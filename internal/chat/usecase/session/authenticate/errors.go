fcb7082865bd8d2bcc85b469d060d49d9bef1ef2
Vladimir Pankov <vdrpkv@gmail.com>
2022-11-10 23:43:09 +0300

    Remove session authenticate usecase * seems like unnecessary detail too

--- internal/chat/usecase/session/authenticate/errors.go
+++
@@ -1,8 +0,0 @@
-package authenticate
-
-import "errors"
-
-var (
-	ErrSessionNotFound  = errors.New("session is not found")
-	ErrSessionNotActive = errors.New("session is not active")
-)
--- internal/chat/usecase/session/authenticate/model/response/response.go
+++
@@ -1,9 +0,0 @@
-package response
-
-// Model is a use case response model
-type Model struct {
-	UserID UserID
-}
-
-// UserID is used to differ one user from another
-type UserID string
