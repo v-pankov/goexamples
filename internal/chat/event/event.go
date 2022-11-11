b3085c8eed0d88fea82bc814212fbfda69e3e283
Vladimir Pankov <vdrpkv@gmail.com>
2022-11-12 02:06:46 +0300

    Add chat message event * as a separate package - don't put it into entity layer * event is not an entity because     * it does not have it's own id     * adding it it's own id opens doors for data duplication in database

---
+++ internal/chat/event/event.go
@@ -0,0 +1,52 @@
+package event
+
+import (
+	"context"
+	"time"
+)
+
+type NewMessage struct {
+	basicEvent
+	MessageID       MessageID
+	MessageContents MessageContents
+}
+
+type basicEvent struct {
+	UserID    UserID
+	SessionID SessionID
+	Timestamp time.Time
+}
+
+type (
+	UserID          string
+	SessionID       string
+	MessageID       int64
+	MessageContents []byte
+)
+
+type Subscriber[Event any] interface {
+	Subscribe(
+		ctx context.Context,
+		subscriptionID SubscriptionID,
+	) (
+		<-chan *Event,
+		error,
+	)
+}
+
+type Unsubscriber[Event any] interface {
+	Unsubscribe(
+		ctx context.Context,
+		subscriptionID SubscriptionID,
+	) error
+}
+
+type SubscriptionID string
+
+type SubscriptionCreator interface {
+	CreateSubscription(ctx context.Context) (SubscriptionID, error)
+}
+
+type SubscriptionDeleter interface {
+	DeleteSubscription(ctx context.Context) (SubscriptionID, error)
+}
